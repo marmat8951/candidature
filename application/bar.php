f52b311bebdf0344048b8e00414d7870256ac6f5
Martin Passard <git@marmat.ovh>
2019-04-01 19:34:03 +0200

    feat: ajout donation bar

---
+++ application/bar.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+        <link href="assets/css/donations-gauge.css" rel="stylesheet">
+	</head>
+	<body>
+        <div id="donations-gauge-wrapper"></div>
+        <script src="assets/js/donations-gauge.js"></script>
+	</body>
+</html>
